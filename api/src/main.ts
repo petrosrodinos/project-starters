4008e160e4449ebfe780d7ad3ef7d50c4145a331
Petros Rodinos <petros.rodinos@yahoo.com>
2026-09-15 07:18:10 +0300

    port

--- api/src/main.ts
+++ api/src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Access-Control-Allow-Origin'],
   });
 
-  await app.listen(3000);
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port);
 }
 bootstrap();
